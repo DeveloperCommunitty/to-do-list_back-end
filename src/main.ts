108bc83cd7754033f2d1bedda0d468c73dd1f132
LPeter-nm <pg.nasmatos@outlook.com>
2025-01-22 09:31:16 -0300

    feat: instalando e configurando a biblioteca de validação de dados para DTOs e configurando o Cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  })
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true
+    }))
 
   const config = new DocumentBuilder()
     .setTitle('API Todo-List')
