f646054a27cf3ecb5817a31d8872c59e2ffa2060
Jose Vitor <josevtrleal@gmail.com>
2025-01-26 17:56:02 -0300

    feat: Adicionado API de usuários (Login Pendente)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './modules/task/task.module';
+import { UserModule } from './modules/user/user.module';
+import { UserController } from './modules/user/user.controller';
 
 @Module({
-  imports: [TaskModule],
-  controllers: [AppController],
+  imports: [TaskModule, UserModule],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
